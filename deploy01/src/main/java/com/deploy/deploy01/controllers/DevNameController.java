a6e047b17eab61f0b1605985b36bc43440ad4861
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-03-19 16:00:41 +0100

    fix

--- deploy01/src/main/java/com/deploy/deploy01/controllers/DevNameController.java
+++ deploy01/src/main/java/com/deploy/deploy01/controllers/DevNameController.java
@@ -1,5 +1,6 @@
 package com.deploy.deploy01.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,11 @@ import java.util.List;
 @RequestMapping("/dev-name")
 public class DevNameController {
 
+    @Value("${devName}")
+    private String devName;
+
     @GetMapping
     public ResponseEntity<String> getDevNam() {
-        String devName = "Chiara";
         return ResponseEntity.ok("DevName: " + devName);
     }
 }
